989baf8b6b91e384844d1dc2f3d778fbbb59c73a
Michael Aaron <michaelaaron@rodete-laptop-imager.corp.google.com>
2018-02-12 23:27:26 -0700

    Initial neovim configuration - its so fast

--- install.sh
+++ install.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 # a bit hacky, but easiest way to force the script to run from the directory where it lives
 cd "${0%/*}"
-echo $(pwd)
 # dependencies
-sudo apt install zsh i3 rxvt-unicode
+sudo apt install git zsh i3 rxvt-unicode
+git submodule init
+git submodule update
 # x11
 ln -sf $(pwd)/x11/xsessionrc ~/.xsessionrc
 ln -sf $(pwd)/x11/xresources ~/.Xresources
@@ -11,7 +12,8 @@ ln -sf $(pwd)/x11/xresources ~/.Xresources
 ln -sf $(pwd)/i3 ~/.i3
 # zsh
 ln -sf $(pwd)/zsh/zshrc ~/.zshrc
-# vim
-ln -sf $(pwd)/vim ~/.vim
+# neovim
+mkdir -p ~/.config/nvim/
+ln -sf $(pwd)/vim/vimrc ~/.config/nvim/init.vim
 # Done
 echo "Develra Dotfiles applied - logout and login to apply changes"
